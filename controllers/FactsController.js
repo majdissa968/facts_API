00bdda1d56fb827acf75e23000f5a336232ca665
majd <majdissa968@gmail.com>
2018-04-23 09:29:49 +0200

    commit

--- controllers/FactsController.js
+++ controllers/FactsController.js
@@ -1,71 +1,66 @@
-const mongoose = require('mongoose')
-const Facts = mongoose.model('Facts')
+const mongoose = require('mongoose');
+const Facts = mongoose.model('Facts');
 
 exports.getFacts = async (req, res) => {
-  const page = req.query.page || 1;
-  const limit = 4;
-  const skip = (page * limit) - limit;
+	const page = req.query.page || 1;
+	const limit = 4;
+	const skip = page * limit - limit;
 
-  const factsPromise = await Facts
-    .find()
-    .skip(skip)
-    .limit(limit)
-    .sort({ artist: 'asc' })
+	const factsPromise = await Facts.find()
+		.skip(skip)
+		.limit(limit)
+		.sort({ artist: 'abc' });
 
-  const countPromise = Facts.count()
+	const countPromise = Facts.count();
 
-  const [facts, count] = await Promise.all([factsPromise, countPromise])
-  const pages = Math.ceil(count / limit)
+	const [facts, count] = await Promise.all([factsPromise, countPromise]);
+	const pages = Math.ceil(count / limit);
 
-  res.json(facts);
+	res.json(facts);
 };
 
 exports.getFactById = async (req, res) => {
-  const fact = await Facts.findOne({ _id: req.params.id })
-  if (!fact) {
-    throw Error('No fact with the given ID!')
-  }
-  res.json(fact)
-}
+	const fact = await Facts.findOne({ _id: req.params.id });
+	if (!fact) {
+		throw Error('No fact !');
+	}
+	res.json(fact);
+};
 
 exports.factsForm = (req, res) => {
-  res.render('factsForm', {
-    title: 'Add an Album',
-    fact: {}
-  })
-}
+	res.render('factsForm', {
+		title: 'Add an Album',
+		fact: {}
+	});
+};
 
 exports.submitFact = async (req, res) => {
-  console.log(req.body)
-  const fact = await (new Facts(req.body)).save()
-  res.redirect('/facts/add')
-  return
-}
+	console.log(req.body);
+	const fact = await new Facts(req.body).save();
+	res.redirect('/facts/add');
+	return;
+};
 
 exports.editFact = async (req, res) => {
-  const fact = await Facts.findOne({ _id: req.params.id })
-  res.render(
-    'factsForm', {
-      title: `Edit ${fact.title}`,
-      fact
-    }
-  )
-}
+	const fact = await Facts.findOne({ _id: req.params.id });
+	res.render('factsForm', {
+		title: `Edit ${fact.title}`,
+		fact
+	});
+};
 
 exports.updateFact = async (req, res) => {
-  const body = req.body
-  body.updated_at = new Date()
+	const body = req.body;
+	body.updated_at = new Date();
 
-  const fact = await Facts.findOneAndUpdate(
-    { _id: req.params.id },
-    body,
-    { new: true}
-  ).exec()
-  res.redirect('/facts')
-}
+	const fact = await Facts.findOneAndUpdate({ _id: req.params.id }, body, {
+		new: true
+	}).exec();
+	res.redirect('/facts');
+};
 
 exports.deleteFact = async (req, res) => {
-  const fact = await Facts.findOne({ _id: req.params.id })
-  await fact.remove()
-  return res.redirect('/facts')
-}
+	const fact = await Facts.findOne({ _id: req.params.id });
+	await fact.remove();
+	return res.redirect('/facts');
+};
